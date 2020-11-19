62147609021b8764abed884b04400f71798af265
oberlin-t <thomas.l.oberlin@gmail.com>
2020-11-19 13:38:19 -0500

    updated init file

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
 (column-number-mode t)
 (size-indication-mode t)
 (fset 'yes-or-no-p 'y-or-n-p)
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
 
 ;;Set Tab width
 (setq-default tab-width 4
@@ -36,8 +37,18 @@
 
 ;;Use Packages
 (use-package doom-themes
-  :ensure t
   :config
   (load-theme 'doom-one t)
-  (doom-themes-visual-bell-config)
 )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(doom-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
