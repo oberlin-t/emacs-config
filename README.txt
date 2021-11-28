2b51628550e12ddfcae0b02fc02f4674924ecdbd
Thomas Oberlin <thomas.l.oberlin@gmail.com>
2021-11-27 22:56:08 -0500

    redid emacs config

---
+++ README.txt
@@ -0,0 +1,17 @@
+Installed Packages and Configurations:
+eglot
+company-mode
+smartparens
+magit
+simpleclip
+zenburn theme
+which-key
+
+Be sure to install languages servers if using eglot.
+
+C-x and C-t swaped for Dvorak keyboard.
+
+Auto save disabled.
+
+
+
--- init.el
+++ init.el
@@ -1,16 +1,18 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;Include package.el
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+;;Configure Packages
+(require 'smartparens-config)
+(smartparens-global-strict-mode)
+
+(add-hook 'after-init-hook 'global-company-mode)
+
+(load-theme 'zenburn t)
+
+(which-key-mode)
 
 ;; Swap “C-t” and “C-x”, so it's easier to type on Dvorak layout
 (keyboard-translate ?\C-t ?\C-x)
@@ -23,80 +25,34 @@
 (blink-cursor-mode -1)
 (setq ring-bell-function 'ignore)
 (setq inhibit-startup-screen t)
+(setq initial-scratch-message nil)
 
 ;;Enable Stuff
-(global-hl-line-mode +1)
-(line-number-mode +1)
 (global-display-line-numbers-mode 1)
-(column-number-mode t)
-(size-indication-mode t)
 (fset 'yes-or-no-p 'y-or-n-p)
-(global-set-key (kbd "C-x k") 'kill-this-buffer)
-(desktop-save-mode 1)
 
-;;Auto Save Settings
-(setq auto-save-default nil)
+;;Disable Auto Save
 (setq make-backup-files nil)
-
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
 
 ;;Set Tab width
 (setq-default tab-width 4
 	      indent-tabs-mode nil)
 
-;;Set contact
+;;Set contat
 (setq user-full-name "Thomas Oberlin"
       user-mail-adress "thomas.l.oberlin@gmail.com")
 
-;;Set Load Paths
-(let ((default-directory  "~/.emacs.d/lisp/"))
-  (normal-top-level-add-subdirs-to-load-path))
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-
-;;Require "use-package"
-(require 'use-package)
-
-;;Use Packages
-
-(use-package gruvbox
-  :config
-  (load-theme 'gruvbox-dark-hard))
-
-
-(use-package ivy
-  :config
-  (ivy-mode t)
-  (setq ivy-height 10))
-
-(use-package swiper
-  )
-(global-set-key (kbd "C-s") 'swiper) 
-
-(use-package simpleclip
-  :config
-  (simpleclip-mode 1))
-
-(use-package company
-  :bind ("C-." . company-complete)
-  :config
-  (global-company-mode))
-
-(use-package which-key
-  :config
-  (which-key-mode))
-
-(use-package prescient)
-(use-package ivy-prescient
-  :config
-  (ivy-prescient-mode))
-(use-package company-prescient
-  :config
-  (company-prescient-mode))
-
-(use-package magit)
-
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(zenburn-theme which-key company eglot magit simpleclip smartparens)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
